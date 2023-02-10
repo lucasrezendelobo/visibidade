44f5c4610f6c390999d1025454bac7442e2106b0
apshadow <117411638+apshadow@users.noreply.github.com>
2023-02-09 22:23:16 -0300

    Create AlphaSub.java

---
+++ src/one/AlphaSub.java
@@ -0,0 +1,19 @@
+package one;
+
+import one.Alpha;
+
+public class AlphaSub extends Alpha {
+	
+	public static void main(String[] args) {
+		
+		Alpha a = new Alpha();
+		a.nome = "Alpha";
+		a.sobrenome = "da Silva";
+		a.pai = "Jose";
+		a.mãe = "Maria";
+		a.apelido = "ND";
+	}
+
+}
+
+//https://docs.oracle.com/javase/tutorial/java/javaOO/accesscontrol.html
